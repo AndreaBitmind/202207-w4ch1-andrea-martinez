d02fc799d6257b45c3036173072aacf805489d03
Andrea Martinez <andrea.maort@gmail.com>
2022-07-25 22:33:29 +0200

    implemented gentlemen data array to app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,40 @@ import Info from "./components/Info/Info";
 import "./App.css";
 import Button from "./components/Button/Button";
 import Gentleman from "./components/Gentleman/Gentleman";
+import { GentlemanProps } from "./types/interfaces";
+
+const gentlemenList: GentlemanProps[] = [
+  {
+    id: 1,
+    name: "Bertin Osborne",
+    status: "Alive",
+    profession: "Youtuber",
+    twitter: "@osbourne",
+    picture: "bertin.jpg",
+    alternativeText: "Osbourne pointing at you",
+    selected: true,
+  },
+  {
+    id: 2,
+    name: "The Farytale",
+    status: "RIP",
+    profession: "Influencer",
+    twitter: "pending",
+    picture: "fary.jpg",
+    alternativeText: "The Fary pointing at you",
+    selected: false,
+  },
+  {
+    id: 3,
+    name: "Julius Churchs",
+    status: "Alive",
+    profession: "Java developer",
+    twitter: "@julius_churchs",
+    picture: "julio.jpg",
+    alternativeText: "Churchs pointing at you",
+    selected: true,
+  },
+];
 
 const App = (): JSX.Element => {
   return (
@@ -14,7 +48,25 @@ const App = (): JSX.Element => {
           <Info />
           <Button />
         </section>
-        <Gentleman />
+        <main className="main">
+          <ul className="gentlemen">
+            {gentlemenList.map((gentleman) => {
+              return (
+                <Gentleman
+                  key={gentleman.id}
+                  alternativeText={gentleman.alternativeText}
+                  id={gentleman.id}
+                  name={gentleman.name}
+                  picture={gentleman.picture}
+                  profession={gentleman.profession}
+                  selected={gentleman.selected}
+                  status={gentleman.status}
+                  twitter={gentleman.twitter}
+                />
+              );
+            })}
+          </ul>
+        </main>
       </div>
     </>
   );
---
+++ src/types/interfaces.ts
@@ -0,0 +1,10 @@
+export interface GentlemanProps {
+  id: number;
+  name: string;
+  status: string;
+  profession: string;
+  twitter: string;
+  picture: string;
+  alternativeText: string;
+  selected: boolean;
+}
