8ddbe8d9ca043878855c32a81e6f85c15b2c023b
Andrea Martinez <andrea.maort@gmail.com>
2022-07-25 20:53:20 +0200

    added gentleman component

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,14 +1,5 @@
 const Info = (): JSX.Element => {
-  return (
-    <>
-      <header className="main-header">
-        <h1 className="main-title">The pointing gentlemen</h1>
-      </header>
-      <section className="controls">
-        <p className="info">0 gentlemen pointing at you</p>
-      </section>
-    </>
-  );
+  return <p className="info">0 gentlemen pointing at you</p>;
 };
 
 export default Info;
