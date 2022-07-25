056f45e43b58ba816c1212c6918e5373a7650a93
Andrea Martinez <andrea.maort@gmail.com>
2022-07-25 19:43:03 +0200

    added button component

---
+++ src/components/Info/Info.tsx
@@ -0,0 +1,14 @@
+const Info = (): JSX.Element => {
+  return (
+    <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+      </header>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+      </section>
+    </>
+  );
+};
+
+export default Info;
